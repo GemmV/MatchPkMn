33c332cb97c7f6b80d8547d4a4a1cc0f14314064
G3MM4 <gemmvaughan@gmail.com>
2018-07-29 02:50:33 +0200

    haciendo y recibiendo promesas

--- js/main.js
+++ js/main.js
@@ -4,61 +4,86 @@ console.log('>> Ready :)');
 
 
 var click4 = document.getElementById('iddelboton1');
-var click8 = document.getElementById('iddelboton2');
-var click16 = document.getElementById('iddelboton3');
+var click6 = document.getElementById('iddelboton2');
+var click8 = document.getElementById('iddelboton3');
 var botonComenzar = document.getElementById('iddelboton4');
+ 
+var urlApi = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
+var cuatro = "4.json";
+var seis = "6.json";
+var ocho = "8.json";
 
-// click4.addEventListener('click', 'hola');
-// click8.addEventListener('click', 'hola');
-// click16.addEventListener('click', 'hola');
-// botonComenzar.addEventListener('click', 'hola');
+var promesa;
+var UL = document.querySelector('.listadecartas');
 
-console.log(click16);
+function comenzar (){
 
-    // if () {
-    //     fetch('https://raw.githubusercontent.com/Adalab/cards-data/master/4.json')
-    //     .then(function(response){
-    //         return document.body.main.response.innerHTML;
-    // })
+if (click4.checked === true) {
+    
+function llamadaApi(){
+    var result = fetch(urlApi + cuatro)
+      .then(function(translate){
+        return translate.json();
+      });
+    return result;
+    }
+    promesa = llamadaApi();
+
+} else if (click6.checked === true){
+    function llamadaApi(){
+    var result = fetch(urlApi + cuatro)
+      .then(function(translate){
+        return translate.json();
+      });
+    return result;
+    }
+    promesa = llamadaApi();
+
+} else if (click8.checked === true){
+    function llamadaApi(){
+    var result = fetch(urlApi + ocho)
+      .then(function(translate){
+        return translate.json();
+      });
+      console.log(result);
+    return result;
+     }
+     promesa = llamadaApi();
+    
+    }
+}
+botonComenzar.addEventListener('click', comenzar);
+
+
+
+
+// function comenzarJuego (urlApi){
+    
+// }
+    // fetch(urlApi + '/4.json')
+    //   .then(function(response){
+    //     return response.json();
+
+    
+//  console.log(comenzarJuego);
+
+
+// // UL.innerHTML = PoKemon;
+
+// // console.log(urlApi);
+// // console.log(PoKemon);
+
+//     // if () {
+//     //  
         
-    // } else if (condition) {
+//     // } else if (condition) {
         
-    // } {
+//     // } {
                 
-    // }
-
-
-// //llamada a la URL BOTON 1
-// fetch('https://raw.githubusercontent.com/Adalab/cards-data/master/4.json')
-//     .then(function(response){
-//         return response.txt();
-// })
-//     .then(function(html){
-//          document.body.innerHTML = html;
-// });
-
-
-// //llamada a la URL BOTON 2
-// fetch('https://raw.githubusercontent.com/Adalab/cards-data/master/8.json')
-//     .then(function(response){
-//         return response.txt();
-// })
-//     .then(function(html){
-//          document.body.innerHTML = html;
-// });
-
-
-// //llamada a la URL BOTON 3
-// fetch('https://raw.githubusercontent.com/Adalab/cards-data/master/16.json')
-//     .then(function(response){
-//         return response.txt();
-// })
-//     .then(function(html){
-//          document.body.innerHTML = html;
-// });
-
-
-
-// //https://raw.githubusercontent.com/Adalab/cards-data/master/NUMERO.json
-
-//var UL = document.querySelector('.listadecartas');
+//     // }
+// // //llamada a la URL BOTON 1
+// // fetch(urlApi + '8.json')
+// //     .then(function(response){
+// //         return response.txt();
+// // })
+// // //https://raw.githubusercontent.com/Adalab/cards-data/master/NUMERO.json
